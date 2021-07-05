3622433b5e90dbbe70828e5e3a83173176751ef3
TheReaper62 <34223146+TheReaper62@users.noreply.github.com>
2021-07-05 10:42:56 +0800

    Roles + Dark Mode Beta

--- docs/document.js
+++ docs/document.js
@@ -1,4 +1,3 @@
-/* global bootstrap: false */
 (function () {
   'use strict'
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
@@ -6,3 +5,27 @@
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
 })()
+
+function dark(){
+    var main = document.getElementById('content-bg');
+    main.style = "background-color:black;color:white;width: 100%;margin-left: 200px;margin-right: 0px;overflow:scroll;";
+    alert("You have entered Dark mode(eperimental). Reload the page to return to Light Mode")
+}
+
+function togglesidebar(){
+    var divider = document.getElementsByClassName('divider')[0];
+    var main = document.getElementById('content-bg');
+    var sidenav = document.getElementById('sidenav')
+
+    if (divider.id === "sidebarexpanded"){
+        main.style = "width: 100%;margin-right: 10px;overflow:scroll;";
+        divider.style = "position:fixed;margin-right: 0px;";
+        sidenav.style = "display:none;"
+        divider.id = "sidebarhidden";
+    }else if (divider.id === "sidebarhidden") {
+        main.style = "width: 100%;margin-left: 200px;margin-right: 0px;overflow:scroll;";
+        divider.style = "position:fixed;margin-left: 190px;";
+        sidenav.style = "width: 190px;position: fixed;"
+        divider.id = "sidebarexpanded";
+    };
+}
